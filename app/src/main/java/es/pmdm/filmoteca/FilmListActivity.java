f5b0f9da8028e7a3b0e58c94a9824c7411bff8be
d4rkl0k <rubenjuancandela06@gmail.com>
2025-11-17 11:07:35 +0100

    Añadir menú en ActionBar con opciones Acerca de y Añadir película
    Menú principal:
    - Creado menu_main.xml con dos opciones de menú.
    - Opción Acerca de: abree AboutActivity mediante un Intent.
    - Opción Añadir película: añade nueva película con datos por defecto al array.
    - La nueva película usará ic_launcher_foreground como imagen por defecto.
    - Implementado onCreateOptionsMenu() para inflar el menú.
    - Implementado onOptionsItemSelected() para manejar eventos del menú.
    - Toast de confirmación al añadir una película nueva.
    
    Internacionalización:
    - Añadidos strings de menu_about, menu_add_film y film_added.
    - Traducciones een español e inglés para todas las opciones del menú.
    
    Actionbar:
    - Cambiado tema de NoActionBar a tema con ActionBar visible.
    - ActionBar muestra el título de la aplicación.
    - Añadido android:fitsSystemWindows=\"true\" en todos los layouts para que el menú no se coma la pantalla.
    
    La película por defecto tendrá los datos:
    - Título: Nueva Película.
    - Director: Director Desconocido.
    - Año: 2025.
    - Formato: Digital.
    - Género: Ciencia Ficción.
    - Comentarios: Película añadida dese el menú. Edita sus datos.
    - El usuario puede editar los datos después desde el listado.

--- app/src/main/java/es/pmdm/filmoteca/FilmListActivity.java
+++ app/src/main/java/es/pmdm/filmoteca/FilmListActivity.java
@@ -2,10 +2,12 @@ package es.pmdm.filmoteca;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -34,6 +36,50 @@ public class FilmListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_about) {
+            openAboutActivity();
+            return true;
+        } else if (id == R.id.menu_add_film) {
+            addNewFilm();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openAboutActivity() {
+        Intent intent = new Intent(FilmListActivity.this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+    private void addNewFilm() {
+        Film newFilm = new Film();
+        newFilm.setTitle("Nueva Película");
+        newFilm.setDirector("Director Desconocido");
+        newFilm.setYear(2025);
+        newFilm.setImbdURL("http://www.imdb.com");
+        newFilm.setFormat(Film.FORMAT_DIGITAL);
+        newFilm.setGenre(Film.GENRE_SCIFI);
+        newFilm.setComments("Película añadida desde el menú. Edita sus datos.");
+        newFilm.setImageResId(R.drawable.ic_launcher_foreground);
+
+        FilmDataSource.films.add(newFilm);
+
+        adapter.notifyDataSetChanged();
+
+        Toast.makeText(this, R.string.film_added, Toast.LENGTH_SHORT).show();
     }
 
     @Override
